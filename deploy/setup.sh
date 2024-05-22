af733438e3138054ed30fa0ab7eb1c2498018f4f
Subha <sshil958@gmail.com>
2024-05-23 02:10:44 +0530

    setup.sh fixed

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# TODO: Set to URL of git repo.
+# Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/chingfo777/profiles-rest-api.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
@@ -13,25 +13,26 @@ locale-gen en_GB.UTF-8
 # Install Python, SQLite and pip
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
 
+# Create project directory and clone the repository
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
+# Set up virtual environment and install dependencies
 python3 -m venv $PROJECT_BASE_PATH/env
-
 $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
 
 # Run migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 
-# Setup Supervisor to run our uwsgi process.
+# Set up Supervisor to run our uWSGI process
 cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
-# Setup nginx to make our application accessible.
+# Set up nginx to make our application accessible
 cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
